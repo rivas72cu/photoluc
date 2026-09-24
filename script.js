b079b3a306ae9102e10d42530b153022c2032a5a
Reinaldo Mirabal <rey_221072@yahoo.com>
2026-09-24 13:56:54 -0500

    crea Photoluc 7ma etapa

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const btnAnterior = document.getElementById("btnAnterior");
 const btnSiguiente = document.getElementById("btnSiguiente");
 const parejas = ["Leticia y Chistian", 
                  "Flor y Paul",
-                ""];
+                 "Roxana y Juan Carlos"];
 const myHeader = document.getElementById("myHeader").textContent;
+const mysubHeader = document.getElementById("mysubHeader").textContent;
 
 let posicionActual = 0;
 
@@ -13,11 +14,21 @@ function mostrarImagen(posicion)
     img.forEach(imagen => imagen.classList.remove("visible"));
     posicionActual = (posicion + img.length) % img.length;
     img[posicionActual].classList.add("visible");
-    if (posicionActual < 32) 
-       {document.getElementById("myHeader").textContent = parejas[0];} 
-    else 
-       {document.getElementById("myHeader").textContent = parejas[1]; }
-    
+    if (posicionActual <= 31) 
+      {document.getElementById("myHeader").textContent = parejas[0];
+       document.getElementById("mysubHeader").textContent = "Matrimonio Civil";
+      }
+    else
+        if (posicionActual > 31 && posicionActual <= 58)
+          {document.getElementById("myHeader").textContent = parejas[1]
+           document.getElementById("mysubHeader").textContent = "Matrimonio Civil";
+          }
+    else
+    if (posicionActual > 58) 
+      {document.getElementById("myHeader").textContent = parejas[2];
+       document.getElementById("mysubHeader").textContent = "Matrimonio Civil y Religioso";
+      }
+
 }
 
 btnAnterior.addEventListener("click", () => {
